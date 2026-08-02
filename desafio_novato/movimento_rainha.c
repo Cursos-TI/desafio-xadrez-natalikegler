34a33d1af76a7fdd429a130c5b1e41fad4096349
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 10:15:00 -0300

    Movimento rainha

---
+++ desafio_novato/movimento_rainha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Simular movimento: Rainha: 8 casas p/ esquerda
+
+    int i;
+
+    for (i = 1; i <= 8; i++)
+    {
+        printf("Movimento %d da rainha: Esquerda\n", i);
+    }
+}
