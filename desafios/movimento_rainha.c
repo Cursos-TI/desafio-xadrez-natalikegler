5a52dd7e63703a8000cde97e1c475b00bb1eb4f8
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 12:31:31 -0300

    Recursividade para movimento da rainha

--- desafios/movimento_rainha.c
+++ desafios/movimento_rainha.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+void movimentar_rainha(int numero_movimentos)
+{
+    if (numero_movimentos > 0)
+    {
+        movimentar_rainha(numero_movimentos - 1);
+        printf("Movimento %d: Esquerda\n", numero_movimentos);
+    }
+}
+
 int main()
 {
 
     // Desafio 1: simular movimento da rainha: 8 casas p/ esquerda
 
     // Desafio 2: use recursividade
-
-        int i;
-
-    for (i = 1; i <= 8; i++)
-    {
-        printf("Movimento %d da rainha: Esquerda\n", i);
-    }
+    int numero_movimentos = 8;
+    printf("Rainha vai se movimentar %dx para a esquerda!\n", numero_movimentos);
+    movimentar_rainha(numero_movimentos);
 }
