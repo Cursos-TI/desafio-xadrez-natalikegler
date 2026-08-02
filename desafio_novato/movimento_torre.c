764d624174eba298fc77fd44a398704391917529
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 10:13:15 -0300

    Movimento Torre

---
+++ desafio_novato/movimento_torre.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // - Simular movimento: Torre: 5 casas p/ direita
+
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Movimento %d da torre: Direita \n", i);
+    }
+}
