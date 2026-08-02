1b00eefb0953635afae874bda5f8073d3844f82b
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 12:53:48 -0300

    Loop do cavalo mais complexo

--- desafios/movimento_cavalo.c
+++ desafios/movimento_cavalo.c
@@ -5,17 +5,19 @@ int main()
 
     // Desafio 1: Simular movimento Cavalo: 2 para baixo, 1 esquerda
 
-    // Desafio 2: loops mais complexos. 2 para cima e 1 direita
+    // Desafio 2: loops mais complexos. 2 para cima e 1 direita (var multiplas e cond avançadas)
 
     int i, j;
+    int numero_movimentacoes = 1;
 
-    for (i = 1; i <= 2; i++)
+    printf("Cavalo vai se movimentar %dx!\n", numero_movimentacoes);
+
+    for (i = 1, j = 1; i <= numero_movimentacoes + 1; i++, j++)
     {
-        printf("Baixo, ");
-        while (i == 2)
+        printf("Cima, ");
+        if (j == 2)
         {
-            printf("Esquerda.\n");
-            i++;
+            printf("Direita. \n");
         }
     }
 }
