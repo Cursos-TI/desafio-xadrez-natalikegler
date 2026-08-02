b5e5b716d85896cae1415da3d135c71341971901
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 10:12:46 -0300

    Movimento Bispo

---
+++ desafio_novato/movimento_bispo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // - Simular movimento Bispo: diagonal (cima e direita)
+
+    int i;
+
+    for (i = 1; i <= 4; i++)
+    {
+        printf("Movimento %d do bispo: Cima, Direita \n", i);
+    }
+}
