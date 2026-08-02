bb26ae868a546870dfff7cc07b169018cdbc5fd2
Nátali Kegler <nkpivato@gmail.com>
2026-08-02 12:41:57 -0300

    Movimentação bispo por loops aninhados

--- desafios/movimento_bispo.c
+++ desafios/movimento_bispo.c
@@ -6,10 +6,18 @@ int main()
 
     // Desafio 2: loops aninhados. Externo vertical e interno horizontal
 
-    int i;
+    int numero_movimentos = 1;
+    int i, j;
 
-    for (i = 1; i <= 4; i++)
+    printf("Bispo vai se movimentar %dx!\n", numero_movimentos);
+
+    for (i = 1; i <= numero_movimentos; i++)
     {
-        printf("Movimento %d do bispo: Cima, Direita \n", i);
+        printf("Movimento %d: Cima e ", i);
+        for (j = 1; j == 1; j++)
+        {
+            printf("Direita\n");
+            j++;
+        }
     }
 }
